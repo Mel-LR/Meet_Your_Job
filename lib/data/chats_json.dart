cf10210b6ad6b85d2bad616a34b84136048cc9ea
Mel-LR <melyne.leray@edu.itescia.fr>
2022-05-24 09:18:19 +0200

    first commit

---
+++ lib/data/chats_json.dart
@@ -0,0 +1,120 @@
+// users story list
+const List chats_json = [
+  {
+    "img": "assets/images/students/img_1.jpeg",
+    "name": "Yanis",
+    "online": true,
+    "story": true,
+  },
+  {
+    "img": "assets/images/students/img_2.jpeg",
+    "name": "Maya",
+    "online": true,
+    "story": false,
+  },
+  {
+    "img": "assets/images/students/img_3.jpeg",
+    "name": "Johanna",
+    "online": true,
+    "story": true,
+  },
+  {
+    "img": "assets/images/students/img_4.jpeg",
+    "name": "Jordan",
+    "online": false,
+    "story": false,
+  },
+  {
+    "img": "assets/images/students/img_5.jpeg",
+    "name": "Mélyne",
+    "online": true,
+    "story": true,
+  },
+  {
+    "img": "assets/images/students/img_6.jpeg",
+    "name": "Emilie",
+    "online": false,
+    "story": true,
+  },
+  {
+    "img": "assets/images/students/img_7.jpeg",
+    "name": "Anne",
+    "online": true,
+    "story": false,
+  },
+  {
+    "img": "assets/images/students/img_8.jpeg",
+    "name": "Marc",
+    "online": true,
+    "story": true,
+  },
+  {
+    "img": "assets/images/students/img_9.jpeg",
+    "name": "Sarah",
+    "online": false,
+    "story": true,
+  },
+  {
+    "img": "assets/images/students/img_10.jpeg",
+    "name": "Estaban",
+    "online": false,
+    "story": true,
+  },
+];
+
+// users message list
+List userMessages = [
+  {
+    "id": 1,
+    "name": "Yanis",
+    "img": "assets/images/students/img_1.jpeg",
+    "online": true,
+    "story": true,
+    "message": "Bonjour M. Cornet.",
+    "created_at": "1:00 pm"
+  },
+  {
+    "name": "Maya",
+    "img": "assets/images/students/img_2.jpeg",
+    "online": true,
+    "story": false,
+    "message": "Bien reçu!",
+    "created_at": "12:00 am"
+  },
+  {
+    "id": 3,
+    "name": "Johanna",
+    "img": "assets/images/students/img_3.jpeg",
+    "online": true,
+    "story": true,
+    "message": "Je vous ai envoyé mon cv par mail.",
+    "created_at": "3:30 pm"
+  },
+  {
+    "id": 4,
+    "name": "Jordan",
+    "img": "assets/images/students/img_4.jpeg",
+    "online": false,
+    "story": false,
+    "message": "Merci pour le retour",
+    "created_at": "9:00 am"
+  },
+  {
+    "id": 5,
+    "name": "Mélyne",
+    "img": "assets/images/students/img_5.jpeg",
+    "online": true,
+    "story": false,
+    "message": "Bonjour, comment allez-vous?",
+    "created_at": "11:25 am"
+  },
+  {
+    "id": 6,
+    "name": "Emilie",
+    "img": "assets/images/students/img_6.jpeg",
+    "online": false,
+    "story": true,
+    "message": "Merci bien, bonne journée! 😄",
+    "created_at": "10:00 am"
+  },
+];
---
+++ lib/data/likes_json.dart
@@ -0,0 +1,8 @@
+const List likes_json = [
+  {"img": "assets/images/students/img_10.jpeg", "active": true},
+  {"img": "assets/images/students/img_2.jpeg", "active": false},
+  {"img": "assets/images/students/img_3.jpeg", "active": false},
+  {"img": "assets/images/students/img_4.jpeg", "active": true},
+  {"img": "assets/images/students/img_1.jpeg", "active": true},
+  {"img": "assets/images/students/img_6.jpeg", "active": true},
+];
